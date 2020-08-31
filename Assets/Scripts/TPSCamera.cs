23f64ac6d07d08632a19b23024a071800b361f80
sumeyyenurozgenc <59045890+sumeyyenurozgenc@users.noreply.github.com>
2020-08-31 23:46:39 +0300

    Add files via upload

---
+++ Assets/Scripts/TPSCamera.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSCamera : MonoBehaviour
+{
+    public float turnSpeed;
+    public Transform player;
+    public float distanceY, distanceZ;
+    private float firstY, firstZ;
+    private Vector3 cameraDistance;
+
+    void Start()
+    {
+        distanceY = 5f;
+        firstY = distanceY;
+        distanceZ = 5f;
+        firstZ = distanceZ;
+        cameraDistance = new Vector3(player.position.x, player.position.y + distanceY, player.position.z + distanceZ);
+    }
+    void LateUpdate()
+    {
+        if(distanceY != firstY)
+        {
+            cameraDistance = new Vector3(player.position.x, player.position.y + distanceY, player.position.z + distanceZ);
+            firstY = distanceY;
+        }
+        if (distanceZ != firstZ)
+        {
+            cameraDistance = new Vector3(player.position.x, player.position.y + distanceY, player.position.z + distanceZ);
+            firstZ = distanceZ;
+        }
+        cameraDistance = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * cameraDistance;
+        transform.position = player.position + cameraDistance;
+        transform.LookAt(player.position);
+    }
+}
---
+++ Assets/Scripts/TPSMoveCube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPSMoveCube : MonoBehaviour
+{
+    public float speed;
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(horizontal * speed * Time.deltaTime, 0, vertical * speed*Time.deltaTime);
+    }
+}
